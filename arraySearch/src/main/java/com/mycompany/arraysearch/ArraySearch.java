f923b60896e58e7d020e3297f9a8aaaedcfa8cd0
Marika <marika.sadowska00@gmail.com>
2026-01-23 15:38:06 +0000

    Adding project folders

---
+++ arraySearch/src/main/java/com/mycompany/arraysearch/ArraySearch.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arraysearch;
+
+/**
+ *
+ * @author Marika
+ */
+public class ArraySearch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ matrixSpiralTraversal/src/main/java/com/mycompany/matrixspiraltraversal/MatrixSpiralTraversal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.matrixspiraltraversal;
+
+/**
+ *
+ * @author Marika
+ */
+public class MatrixSpiralTraversal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
