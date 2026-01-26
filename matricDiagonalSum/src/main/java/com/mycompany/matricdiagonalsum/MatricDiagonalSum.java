aad0502fd83ddd50550596a9a0509006882df4d8
Marika <marika.sadowska00@gmail.com>
2026-01-26 16:17:50 +0000

    Reading user input and inserting it into a 2d array , verifying the format of the user input matches the required format given to user in the command line.

--- matricDiagonalSum/src/main/java/com/mycompany/matricdiagonalsum/MatricDiagonalSum.java
+++ matricDiagonalSum/src/main/java/com/mycompany/matricdiagonalsum/MatricDiagonalSum.java
@@ -31,5 +31,42 @@ public class MatricDiagonalSum {
             so.nextLine();
         } while (matrixSize < 0);
         
+        int[][] userMatrix = new int[matrixSize][];
+        int[] matrixLine = new int[matrixSize];
+        
+        for (int row = 0; row < userMatrix.length; row++){
+            System.out.println("Enter row " + (row + 1) + " in the format: x y z...");
+            
+            String tempInput  = so.nextLine();
+            
+            while (!tempInput.matches("^\\d+( \\d+){" + (matrixSize - 1) + "}$")){
+                System.out.println("Invalid format. Please enter exactly " + matrixSize + " numbers separated by spaces:");
+                tempInput  = so.nextLine();
+            }
+            
+            String[] tempArray = tempInput.split("\\s+");
+            
+            int[] currentRow = new int[matrixSize];
+            
+            for (int i = 0; i < tempArray.length; i++){
+                currentRow[i] = Integer.parseInt(tempArray[i]);
+            }
+            
+            userMatrix[row] = currentRow;
+         }
+        
+        System.out.println("Your matrix table is ready:");
+        
+        for (int row = 0; row <  userMatrix.length; row++){
+            for (int col = 0; col < userMatrix[row].length; col++){
+                System.out.printf("%5d", userMatrix[row][col]);
+            }
+            System.out.println("");
+        }
+
     }
+    
+//    static int[] stringToIntArr(String userInput){
+//        String userInputArray = userInput.split(" ");
+//    }
 }
