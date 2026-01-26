fd72ad005cad32fa23b9cf31a85edccabbecb0bb
Marika <marika.sadowska00@gmail.com>
2026-01-26 15:00:52 +0000

    Reading user input for matric size, verifying that size is a valid, positive number before saving the input.

--- matricDiagonalSum/src/main/java/com/mycompany/matricdiagonalsum/MatricDiagonalSum.java
+++ matricDiagonalSum/src/main/java/com/mycompany/matricdiagonalsum/MatricDiagonalSum.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.matricdiagonalsum;
+import java.util.Scanner;
 
 /**
  *
@@ -12,5 +13,23 @@ public class MatricDiagonalSum {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        Scanner so = new Scanner(System.in);
+        
+        int matrixSize;
+        
+        do {
+            System.out.println("Enter your matrix size: ");
+            
+            while (!so.hasNextInt()){
+                System.out.println("The size must be a valid positive number.");
+                so.next();
+            }
+            
+            matrixSize = so.nextInt();
+            // Adding this line as nextInt() leaves a newline character in the buffer and can prevent reading the next input
+            so.nextLine();
+        } while (matrixSize < 0);
+        
     }
 }
