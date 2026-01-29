7ba140c179c9098daf7b34a33aea17c4dc415c96
Marika <marika.sadowska00@gmail.com>
2026-01-29 13:09:08 +0000

    Reading user input for their matrix values, and saving them line by line.

--- matrixSpiralTraversal/src/main/java/com/mycompany/matrixspiraltraversal/MatrixSpiralTraversal.java
+++ matrixSpiralTraversal/src/main/java/com/mycompany/matrixspiraltraversal/MatrixSpiralTraversal.java
@@ -12,8 +12,6 @@ import java.util.Scanner;
 public class MatrixSpiralTraversal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
         Scanner sc = new Scanner(System.in);
         
         String regex = "^[1-9]\\d*\\s+[1-9]\\d*$";
@@ -40,6 +38,29 @@ public class MatrixSpiralTraversal {
             rowValue = dimensions[0];
             colValue = dimensions[1]; 
         } while (rowValue < 0 && colValue < 0);
+        
+        int[][] userMatrix = new int[rowValue][colValue];
+        
+        for (int row = 0; row < userMatrix.length; row++){
+            System.out.println("Enter row " + (row + 1) + " in the format: x y z...");
+            
+            String tempRow = sc.nextLine();
+            
+            while (!tempRow.matches("^\\d+( \\d+){" + (colValue - 1) + "}$")){
+                System.out.println("Invalid format. Please enter exactly " + colValue + " numbers separated by spaces:");
+                tempRow = sc.nextLine();
+            }
+            
+            String[] tempRowArr = tempRow.split("\\s+");
+            
+            int[] currentRow = new int[colValue];
+            
+            for (int i = 0; i < tempRowArr.length; i++){
+                currentRow[i] = Integer.parseInt(tempRowArr[i]);
+            }
+            
+            userMatrix[row] = currentRow;
+        }
         
         System.out.println("Your row value is " + rowValue + " and column value is " + colValue);
     }
